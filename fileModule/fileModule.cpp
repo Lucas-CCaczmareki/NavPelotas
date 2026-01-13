932912fb0faca5e79754a6265f2afec6d892fe90
godinhojoao <godinhojoao2003@gmail.com>
2026-01-12 22:24:29 -0300

    feat: read label to node file line by line and get correct format

--- fileModule/fileModule.cpp
+++ fileModule/fileModule.cpp
@@ -16,8 +16,45 @@ namespace fileModule
     std::string line;
     while (std::getline(inFile, line))
     {
-      std::cout << "line: " << line << "\n";
+      int isReadingLabel = 0;
+      std::string node;
+      std::string label;
+      for (int i = 0; i < line.length(); i++)
+      {
+        if (line[i] == '"' || line[i] == '{' || line[i] == '}' || line[i] == ',')
+        {
+          continue;
+        }
+
+        // move to read label moment
+        if (line[i] == ':')
+        {
+          isReadingLabel = 1;
+          continue;
+        }
+
+        if (isReadingLabel)
+        {
+          label += line[i];
+        }
+        else
+        {
+          node += line[i];
+        }
+      }
+
+      // we can have TWO or MORE nodes for same label
+      // trie path -> label
+      // each label has its payloads -> int nodes[] OR better std::vector<int> nodes;
+      if (!(label.empty() && node.empty()))
+      {
+        std::cout << "node: " + node << "\n";
+        std::cout << "label: " + label << "\n";
+        std::cout << "\n";
+        std::cout << "\n";
+      }
     }
+
     inFile.close();
   }
 
--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main()
 {
   fileModule::readNodesToLabel();
 
-  trie::foo();
+  // trie::foo();
 
   return 0;
 }
