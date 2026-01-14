8fa6c1507a50567f4a17ddbcbee281f9dad7a137
Lucas-CCaczmareki <lucascaczmareki@gmail.com>
2026-01-14 11:46:40 -0300

    installing json library

--- src/main.cpp
+++ src/main.cpp
@@ -1,67 +1,12 @@
 #include <iostream>
+#include <fstream> //provavelmente pra acessar files
 #include <vector>
 #include <string>
-#include "Graph.h"
+// #include "Graph.h"
+#include "json.hpp"
+
 
 int main() {
-    std::cout << "Hello C++!\n";
-
-    // // int x;
-    // // float b;
-
-    // std::vector<int> v;
-
-    // for(int i = 0; i < 10; i++) {
-    //     // std::cout << "Digite um numero: ";
-    //     v.push_back(i);
-    // }
-
-    // // int var : container
-    // // a variavel recebe o próximo valor do container a cada iteração
-    // // equivale a percorrer o vetor do inicio ao fim
-    // for(int x : v) {
-    //     std::cout << x;
-    //     if (x < static_cast<int>(v.size()) - 1) {
-    //         std::cout << ", ";
-    //     }
-    // }
-    
-    // std::string linha;
-
-    // std::cin >> linha;  //aqui lê até o espaço
-    // std::cin.ignore();  // consome um caracter do buffer, nesse caso o espaço
-    // std::cout << linha << "\n";
-
-    // //aqui vai ler o resto do buffer "aranha\n" e vai printas
-    // std::getline(std::cin, linha); //aqui lê até o \n
-    // std::cout << linha;
-
-    
-    // std::cout << "Type an int, then type a float.\n";
-    // std::cin >> a >> b;
-    // std::cout << "You typed: " << a << ", " << b << "\n";
-
-    // v.push_back(a);
-    // v.push_back(b); //aqui é pra dar erro ou truncar
-
-    // std::cout << "v[0]: " << v[0] << "\nv[1]: " << v[1] << "\n";
-
-    std::cout << "\n\n";
-
-    Graph g(5);
-    g.addEdge(0, 1, 2);
-    g.addEdge(0, 3, 3);
-    g.addEdge(1, 2, 3);
-    g.addEdge(3, 1, 1);
-
-    std::vector<Graph::Edge> a_edges;
-    a_edges = g.neighbours(0);
-
-    for ( Graph::Edge& e : a_edges ) {
-        std::cout << e.to << " " << e.weight << "\n";
-    }
-
-    std::cout << "Tamanho: " << g.size() << "\n";
-
-    return 0;
+    using json = nlohmann::json; //só pra escrever menos coisa. Faz json ser uma keywork que é nlohmann::json
+    std::cout << "Hello World\n";
 }
